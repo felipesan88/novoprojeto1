22d13bfb2501dd7058d9a1de41f82f425fad15af
felipe.smsouza2 <felipe.smsouza2@AME0635272W10-1>
2023-02-15 11:00:40 -0300

    Adicionar arquivos de projeto.

---
+++ novoprojeto1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace novoprojeto1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string cpf;
+            Console.WriteLine("Escreva seu cpf: ");
+            Console.ReadLine();
+
+
+        }
+    }
+}
